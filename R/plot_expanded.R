1711949fe7e0ae9b47bf080222a871297c1981a8
eric-ward <eric.ward@noaa.gov>
2017-04-25 09:33:19 -0700

    update examples / documentation

--- R/plot_expanded.R
+++ R/plot_expanded.R
@@ -3,13 +3,14 @@
 #' @param fitted_model Data and fitted model returned from bycatch_expansion()
 #' @param xlab X-axis label for plot
 #' @param ylab Y-axis label for plot
+#' @param include_points whether or not to include raw bycatch events on plots, defaults to FALSE
 #'
 #' @return plot called from ggplot
 #'
 #' @export
 #' @import ggplot2
 #' @import viridis
-plot_expanded = function(fitted_model, xlab = "Time", ylab="Events") {
+plot_expanded = function(fitted_model, xlab = "Time", ylab="Events", include_points=FALSE) {
 
   df = data.frame("time" = fitted_model$data$time,
     "mean" = apply(fitted_model$expanded_estimates, 2, mean),
@@ -20,9 +21,9 @@ plot_expanded = function(fitted_model, xlab = "Time", ylab="Events") {
   g1 = ggplot(df, aes(time, mean)) +
     geom_ribbon(aes(ymin = low, ymax=high), fill="blue", alpha=0.3) +
     geom_line(color = "blue") +
-    geom_point(aes(time, obs), size=2, color = "blue") +
     xlab(xlab) + ylab(ylab) + theme_bw() +
     theme(panel.border = element_blank(), panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+  if(include_points==TRUE) g1 = g1 + geom_point(aes(time, obs), size=2, color = "blue")
   return(g1)
 }
--- R/plot_fitted.R
+++ R/plot_fitted.R
@@ -3,13 +3,14 @@
 #' @param fitted_model Data and fitted model returned from bycatch_expansion()
 #' @param xlab X-axis label for plot
 #' @param ylab Y-axis label for plot
+#' @param include_points whether or not to include raw bycatch events on plots, defaults to FALSE
 #'
 #' @return plot called from ggplot
 #'
 #' @export
 #' @import ggplot2
 #' @import viridis
-plot_fitted = function(fitted_model, xlab = "Time", ylab="Events") {
+plot_fitted = function(fitted_model, xlab = "Time", ylab="Events", include_points=FALSE) {
 
   lambda = rstan::extract(fitted_model$fitted_model, c("lambda"))$lambda
 
@@ -28,9 +29,9 @@ plot_fitted = function(fitted_model, xlab = "Time", ylab="Events") {
   g1 = ggplot(df, aes(time, mean)) +
     geom_ribbon(aes(ymin = low, ymax=high), fill="blue", alpha=0.3) +
     geom_line(color="blue") +
-    geom_point(aes(time, obs), size=2, color="blue") +
     xlab(xlab) + ylab(ylab) + theme_bw() +
     theme(panel.border = element_blank(), panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+  if(include_points==TRUE) g1 = g1 + geom_point(aes(time, obs), size=2, color="blue")
   return(g1)
 }
