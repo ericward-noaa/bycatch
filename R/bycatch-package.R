402d42eb4323404e2e804498fa884c3d78193df1
ericward-noaa <eric.ward@noaa.gov>
2023-07-05 08:57:41 -0700

    rstantools

--- R/bycatch-package.R
+++ R/bycatch-package.R
@@ -8,10 +8,9 @@
 #' @useDynLib bycatch, .registration = TRUE
 #' @import methods
 #' @import Rcpp
-#' @import rstantools
 #' @importFrom rstan sampling
 #'
 #' @references
-#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#' Stan Development Team (2023). RStan: the R interface to Stan. R package version 2.21.8. https://mc-stan.org
 #'
 NULL
